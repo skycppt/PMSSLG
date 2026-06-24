2fd4b54f4a101cdf01e43ce2394bd9e84a18f701
skycppt <sujit.arcade.jgec@gmail.com>
2026-06-24 17:42:43 +0530

    Get all books API completed

--- backend/src/controllers/bookController.js
+++ backend/src/controllers/bookController.js
@@ -16,3 +16,19 @@ export const createBook = async (req, res) => {
     });
   }
 };
+
+export const getAllBooks = async (req, res) => {
+  try {
+
+    const books = await Book.find();
+
+    res.status(200).json(books);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
 
-import { createBook } from "../controllers/bookController.js";
+import {
+  createBook,
+  getAllBooks,
+} from "../controllers/bookController.js";
 
 import protect from "../middleware/authMiddleware.js";
 import authorize from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
+
+router.get("/", getAllBooks);
 
 router.post(
   "/",
