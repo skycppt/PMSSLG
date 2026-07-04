e2865bcefb3537a5764bd405d1d870692248c1ba
skycppt <sujit.arcade.jgec@gmail.com>
2026-07-04 10:12:02 +0530

    Fix Book model import casing

--- backend/src/controllers/bookController.js
+++ backend/src/controllers/bookController.js
@@ -1,4 +1,4 @@
-import Book from "../models/Book.js";
+import Book from "../models/book.js";
 import StockHistory from "../models/StockHistory.js";
 
 export const createBook = async (req, res) => {
