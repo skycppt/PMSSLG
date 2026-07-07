f72a00878d24d3311ca97814122ed5559b669bd1
skycppt <sujit.arcade.jgec@gmail.com>
2026-07-07 20:04:24 +0530

    full name show option

--- backend/src/services/dashboard/recentSalesService.js
+++ backend/src/services/dashboard/recentSalesService.js
@@ -1,26 +1,19 @@
 import BookSale from "../../models/BookSale.js";
 
+
+
+
 export async function getRecentSales() {
 
-    const recentSales =
-        await BookSale.find()
-            .sort({
-                createdAt: -1
-            })
-            .limit(5)
-            .populate(
-                "soldBy",
-                "fullName"
-            );
+    const recentSales = await BookSale.find()
+        .sort({ createdAt: -1 })
+        .limit(5)
+        .populate("member", "fullName")
+        .populate("soldBy", "fullName");
 
     return {
-
         sales: {
-
             recentSales
-
         }
-
     };
-
 }
